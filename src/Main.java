d1d45a749edd720d1f2b2fd0ce2d9924f1b1caab
hoangtuanc02 <tuan4695hatinh@gmail.com>
2025-04-10 15:31:48 +0700

    .

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Student[] students = {
                 new Student(001, "Nguyen Van A", "A@example.com", 20, 10.0),
-                new Student(002, "Tran Thi B", "B@example.com", 21, 4.75),
-                new Student(003, "Le Van C", "C@example.com", 19, 7.25),
-                new Student(004, "Pham Van D", "D@example.com", 22, 8.0),
-                new Student(005, "Nguyen Thi E", "E@example.com", 20, 5.25)
+                new Student(002, "Bui Van Nong", "ElongMUsk@example.com", 21, 4.75),
+                new Student(003, "Pham Trung Hiu", "Hiuthu2@example.com", 19, 7.25),
+                new Student(004, "Nguyen Hoang Tuan", "Tuandeptrai@example.com", 22, 8.0),
+                new Student(005, "Vu Thi Kieu Anh", "KieuAnhXinh@example.com", 20, 5.25)
         };
 
         int totalAge = 0;
@@ -23,7 +23,6 @@ public class Main {
         }
         System.out.println("Number of students passed: " + passedCount);
 
-        //thong tin cua Nguyen Van A
         for (Student s : students) {
             if (s.fullName.equalsIgnoreCase("Nguyen Van A")) {
                 System.out.println("Email of Nguyen Van A: " + s.email);
